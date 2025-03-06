2c61e50313041dbda7ebc18d18b27c48a041f4df
hadsome-Droid <xkumbhax@gmail.com>
2025-03-06 13:51:22 +0300

    refactor users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, ConflictException, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDTO } from './dto/login-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post('register')
+  async register(@Body() createUserDto: CreateUserDto) {
+    try {
+      return await this.usersService.register(createUserDto);
+    } catch (error) {
+      throw new ConflictException(error.message);
+    }
+  }
+
+  @Post('login')
+  async login(@Body() loginUserDto: LoginUserDTO) {
+    return this.usersService.login(loginUserDto);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
+import { LoginUserDTO } from './dto/login-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async register(createUserDto: CreateUserDto) {
+    const { password, name, email } = createUserDto;
+
+    // Проверяем, существует ли пользователь с таким email
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email },
+    });
+    if (existingUser) {
+      throw new ConflictException('User with email already exists');
+    }
+
+    // Хешируем пароль
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Создаем пользователя
+    const user = await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return user;
+  }
+
+  async login(loginUserDto: LoginUserDTO) {
+    const { password, email } = loginUserDto;
+
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+    if (!user) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid password or password ');
+    }
+
+    return user;
+  }
+}
